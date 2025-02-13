c73e7e1712e50478832f74c4abadc5e2dc9b3c90
Sayan <sayanguha.underpinservices@gmail.com>
2025-02-13 15:52:06 +0530

    initial commit

---
+++ Assets/Scripts/Functionality/PayoutHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Best.HTTP.SecureProtocol.Org.BouncyCastle.Asn1.Misc;
+using TMPro;
+using UnityEngine;
+
+public class PayoutHandler : MonoBehaviour
+{
+    [SerializeField] internal int id;
+    [SerializeField] internal GameObject imageAnimation;
+    [SerializeField] internal TMP_Text text;
+
+}
